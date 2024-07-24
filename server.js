fd5edc3fdffcfb855f7460fae9845b1d81a0f351
sayedalali1 <sayedalali1@gmail.com>
2024-07-24 14:47:49 +0300

    updated server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+// new middleware
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -35,6 +39,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn); // use new isSignedIn middleware here
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
